8655a716d1bac8488be1f2167d0ae6e55b962077
Shai Yaakovi <shaiy310@gmail.com>
2018-08-01 11:17:35 +0000

    another step toward full convertion...

--- cam_webserver/website/Cam/views.py
+++ cam_webserver/website/Cam/views.py
@@ -9,6 +9,25 @@ from django.views import generic
 # from .models import Question, Choice
 # Create your views here.
 
+
+is_simple = False
+def get_display_style():
+    global is_simple
+    if is_simple:
+        return 'style="display:none;"'
+    else:
+        return ''
+
+
+def user_buttons():
+    btn_format = '\<input id=\"%s\" type="button" value="%s" onclick="send_cmd(\'sy %s\')" class=btn btn-primary" >'
+    user_buttons = (
+        ('Start Tank', './tank.sh'),
+    )
+    buttons = '\r\n'.join(btn_format % (name, name, macro) for name, macro in user_buttons)
+    return '<div class="container-fluid text-center"> %s </div>\r\n' % (buttons, )
+
+
 class IndexView(generic.ListView):
     template_name = 'Cam/index.html'
     #context_object_name = 'latest_question_list'
@@ -16,10 +35,15 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return []
 
+
 def test(request, ):
+    print '~~~~%s' % (request.GET.get('toggle_display'), )
     return render(request, 'Cam/index.html', {
         'CAM_STRING' : 'RPi Cam Control v6.4.24 mycam@%s' % ('RaspPi', ),
         'mjpegmode' : 1,
         'video_fps' : 2,
         'divider' : 1,
-        })
+        'simple' : get_display_style(),
+        'user_buttons' : (('Start Tank', './tank.sh'), ),
+        'toggle_str' : 'Simple' if not is_simple else 'Full',
+    })
